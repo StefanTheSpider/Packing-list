7b33c9350fd83ad8cc9e0ec9f52c3cc74c6bce20
StefanTheSpider <stefantuaevgmail.com>
2024-03-26 16:56:34 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Form />
+            <Packinglist />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+function Header() {
+    return <h1>✈️ Far Away 🏝️</h1>
+}
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>something</h3>
+            <input></input>
+            <input></input>
+            <button></button>
+        </div>
+    )
+}
+function Packinglist() {
+    return <div className="list">hier kommt ein Bild rein</div>
+}
+function Footer() {
+    return (
+        <footer className="stats">
+            <em>You have x items on your list, and you alrady packed x (x%)</em>
+        </footer>
+    )
+}
